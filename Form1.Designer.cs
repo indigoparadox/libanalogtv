4b2ce500a2055ec9ee50959aeba9670b95e1aa50
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2015-10-02 17:51:27 -0400

    Attempting to get interop to draw.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,6 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
         }
