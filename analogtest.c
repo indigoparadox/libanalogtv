18bf6609120c7b547ec9d9230454252934d28c49
indigoparadox <indigo.repo@interfinitdynamics.info>
2015-10-07 15:33:12 -0400

    C library with tester now works (very slowly) in Windows.

--- analogtest.c
+++ analogtest.c
@@ -1,13 +1,89 @@
 
 #include <stdint.h>
+#ifdef WIN32
+#include <Windows.h>
+#else
 #include <X11/Xlib.h>
+#endif
 #include "analogtv.h"
 
+analogtv* tv;
+analogtv_reception* reception;
+
 #ifdef WIN32
+#define IDT_TIMER1 10001
+
+HWND win;
+
+LRESULT CALLBACK _TVWinProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT sPaint;
+	HGDIOBJ hfDefault;
+	HWND hWndButton;
+
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_CREATE:
+		break;
+
+	case WM_TIMER:
+		analogtv_reception_update(reception);
+		analogtv_draw(tv, 0.04, &reception, 1);
+		break;
+
+		/*
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &sPaint);
+		_IPWinBGPaint(hWnd, hdc);
+		EndPaint(hWnd, &sPaint);
+		goto cleanup;
+		*/
+}
+
+cleanup:
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nShowCmd) {
+
+	WNDCLASSEX winclass;
+	HBRUSH hBrush;
+	ZeroMemory(&winclass, sizeof(WNDCLASSEX));
+
+	hBrush = CreateSolidBrush(RGB(0, 0, 0));
+
+	winclass.cbSize = sizeof(WNDCLASSEX);
+	winclass.hbrBackground = hBrush;
+	winclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	winclass.hInstance = hInst;
+	winclass.lpfnWndProc = (WNDPROC)_TVWinProc;
+	winclass.lpszClassName = "AnalogTVTestClass";
+	winclass.style = CS_HREDRAW | CS_VREDRAW;
+
+	int result = RegisterClassEx(&winclass);
+	if (!result) {
+		/* TODO: Display error. */
+		GetLastError();
+		MessageBox(NULL, "Error registering window class.", "Error", MB_ICONERROR | MB_OK);
+		//nRetVal = 1;
+		//goto cleanup;
+		return 1;
+	}
+
+	win = CreateWindowEx(
+		NULL, "AnalogTVTestClass", "Analog TV",
+		WS_OVERLAPPED | WS_MINIMIZEBOX | WS_CAPTION | WS_SYSMENU,
+		100, 100, 800, 600, NULL, NULL, hInst, NULL
+	);
+
+	ShowWindow( win, nShowCmd );
 
 #else
-
-int main( void ) {
+int main(void) {
    Display* dpy;
    Window rootwin;
    Window win;
@@ -38,7 +114,9 @@ int main( void ) {
 
    XMapWindow( dpy, win );
 
-   analogtv_reception* reception = calloc( 1, sizeof( analogtv_reception ) );
+#endif
+
+   reception = calloc( 1, sizeof( analogtv_reception ) );
    analogtv_input* inp = calloc( 1, sizeof( analogtv_input ) );
    int field_ntsc[4] = { 0 };
    uint8_t pixels[32][32];
@@ -49,7 +127,11 @@ int main( void ) {
       }
    }
 
-   analogtv* tv = analogtv_allocate( dpy, win );
+#ifdef WIN32
+   tv = analogtv_allocate( win );
+#else
+   tv = analogtv_allocate( dpy, win );
+#endif
 
    analogtv_set_defaults( tv, "" );
    analogtv_setup_sync( inp, 1, 0 );
@@ -66,6 +148,19 @@ int main( void ) {
                         ANALOGTV_TOP, ANALOGTV_BOT,
                         field_ntsc );
 
+#ifdef WIN32
+   SetTimer(win, IDT_TIMER1, 1000, (TIMERPROC)NULL);
+
+   MSG msg;
+
+   ZeroMemory(&msg, sizeof(MSG));
+
+   /* Message loop. */
+   while (GetMessage(&msg, NULL, 0, 0)) {
+	   TranslateMessage(&msg);
+	   DispatchMessage(&msg);
+   }
+#else
    while( 1 ) {
       analogtv_reception_update( reception );
       analogtv_draw( tv, 0.04, &reception, 1 );
@@ -80,9 +175,7 @@ int main( void ) {
    }
 
    XCloseDisplay( dpy );
+#endif
 
    return 0;   
 }
-
-#endif
-
