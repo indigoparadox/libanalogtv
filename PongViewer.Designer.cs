0bc97c1563baeb4ff0c217b8c45628a83803fe7d
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2015-10-19 15:32:08 -0400

    PongViewer demo now basically works (though it's pretty rough and performance is poor).

--- PongViewer.Designer.cs
+++ PongViewer.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace AnalogTVTest {
-    partial class Form1 {
+    partial class PongViewer {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -24,6 +24,12 @@
         /// </summary>
         private void InitializeComponent() {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -33,23 +39,72 @@
             this.panel1.Size = new System.Drawing.Size(640, 480);
             this.panel1.TabIndex = 0;
             // 
-            // Form1
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // PongViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.panel1);
-            this.Name = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "PongViewer";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.Load += new System.EventHandler(this.PongViewer_Load);
+            this.Resize += new System.EventHandler(this.PongViewer_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- PongViewer.cs
+++ PongViewer.cs
@@ -12,29 +12,33 @@ using System.Runtime.InteropServices;
 using System.Diagnostics;
 
 namespace AnalogTVTest {
-    public partial class Form1 : Form {
+    public partial class PongViewer : Form {
 
-        private static readonly int PONG_WIDTH = 30;
-        private static readonly int PONG_HEIGHT = 30;
-        private static readonly int PONG_MAX_X = AnalogTV.ANALOGTV_VIS_LEN - PONG_WIDTH;
-        private static readonly int PONG_MAX_Y = AnalogTV.ANALOGTV_VISLINES - PONG_HEIGHT;
+        //private static readonly int PONG_WIDTH = 30;
+        //private static readonly int PONG_HEIGHT = 30;
+        //private static readonly int PONG_MAX_X;
+        //private static readonly int PONG_MAX_Y;
 
         //private Graphics g;
         //private AnalogTV.analogtv tvPtr;
         private IntPtr tvPtr;
         //private AnalogTV.analogtv_reception rec;
         private IntPtr recPtr;
-        private int[][] pixels;
-        private int[] ntsc;
         //private AnalogTV.analogtv_input inputPtr;
         private IntPtr inputPtr;
+        private IntPtr pongBitmap = IntPtr.Zero;
 
         private int pongX = 0;
         private int pongY = 0;
         private int pongXInc = 10;
         private int pongYInc = 10;
 
-        public Form1() {
+        private uint pongWidth = 10;
+        private uint pongHeight = 10;
+        private uint pongMaxX = AnalogTV.ANALOGTV_VIS_LEN - 10;
+        private uint pongMaxY = AnalogTV.ANALOGTV_VISLINES - 10;
+
+        public PongViewer() {
             InitializeComponent();
         }
 
@@ -50,21 +54,37 @@ namespace AnalogTVTest {
 
             AnalogTV.analogtv_color( 5, field_ntsc );
 
-            //AnalogTV.analogtv_draw_solid( this.inputPtr,
-            AnalogTV.analogtv_draw_solid( this.inputPtr,
-                AnalogTV.ANALOGTV_VIS_START + x, AnalogTV.ANALOGTV_VIS_START + x + PONG_WIDTH,
-                AnalogTV.ANALOGTV_TOP + y, AnalogTV.ANALOGTV_TOP + y + PONG_HEIGHT,
-                field_ntsc );
+            if( IntPtr.Zero == this.pongBitmap) {
+                AnalogTV.analogtv_draw_solid( this.inputPtr,
+                    AnalogTV.ANALOGTV_VIS_START + x, (int)(AnalogTV.ANALOGTV_VIS_START + x + this.pongWidth),
+                    AnalogTV.ANALOGTV_TOP + y, (int)(AnalogTV.ANALOGTV_TOP + y + this.pongHeight),
+                    field_ntsc );
+            } else {
+                AnalogTV.analogtv_draw_image( this.inputPtr,
+                    this.pongBitmap, AnalogTV.ANALOGTV_VIS_START + x, AnalogTV.ANALOGTV_TOP + y,
+                    this.pongWidth, this.pongHeight );
+            }
 
             //AnalogTV.analogtv_reception_reallocate( this.recPtr, this.inputPtr );
         }
 
-        private void Form1_Load( object sender, EventArgs e ) {
+        public void SetPongImage( string pathIn ) {
+            this.pongX = 0;
+            this.pongY = 0;
+            uint widthIn = 0, heightIn = 0;
+            AnalogTV.analogtv_load_bitmap( pathIn, out this.pongBitmap, out widthIn, out heightIn );
+            this.pongWidth = widthIn;
+            this.pongHeight = heightIn;
+            this.pongMaxX = AnalogTV.ANALOGTV_VIS_LEN - widthIn;
+            this.pongMaxY = AnalogTV.ANALOGTV_VISLINES - heightIn;
+        }
+
+        private void PongViewer_Load( object sender, EventArgs e ) {
 
             //this.Width = 640;
             //this.Height = 480;
 
-            this.ntsc = new int[4] { 0, 0, 0, 0 };
+            int[] ntsc = new int[4] { 0, 0, 0, 0 };
             /*
             this.pixels = new int[this.Width][];
             for( int i = 0 ; this.Height > i ; i++ ) {
@@ -99,11 +119,13 @@ namespace AnalogTVTest {
 
             this.recPtr = AnalogTV.analogtv_reception_allocate( 2.0f, inputPtr );
 
+            /*
             Timer TPaint = new Timer();
             TPaint.Interval = 50;
             TPaint.Tick += this.TPaint_Tick;
             TPaint.Enabled = true;
             TPaint.Start();
+            */
 
             Timer TPong = new Timer();
             TPong.Interval = 50;
@@ -112,17 +134,11 @@ namespace AnalogTVTest {
             TPong.Start();
         }
 
-        private void TPaint_Tick( Object sender, EventArgs e ) {
-            AnalogTV.analogtv_reception_update( this.recPtr );
-
-            AnalogTV.analogtv_draw( this.tvPtr, 0.04, ref this.recPtr, 1 );
-        }
-
         private void TPong_Tick( Object sender, EventArgs e ) {
-            if( PONG_MAX_X <= this.pongX || 0 > this.pongX ) {
+            if( this.pongMaxX < this.pongX || 0 > this.pongX ) {
                 this.pongXInc *= -1;
             }
-            if( PONG_MAX_Y <= this.pongY || 0 > this.pongY ) {
+            if( this.pongMaxY < this.pongY || 0 > this.pongY ) {
                 this.pongYInc *= -1;
             }
 
@@ -130,12 +146,28 @@ namespace AnalogTVTest {
             this.pongY += this.pongYInc;
 
             this.DrawPong( this.pongX, this.pongY );
+
+            AnalogTV.analogtv_reception_update( this.recPtr );
+
+            AnalogTV.analogtv_draw( this.tvPtr, 0.04, ref this.recPtr, 1 );
         }
 
-        private void Form1_Resize( object sender, EventArgs e ) {
-            this.Width = (this.Width / 12) * 12;
+        private void PongViewer_Resize( object sender, EventArgs e ) {
             if( IntPtr.Zero != this.tvPtr ) {
                 AnalogTV.analogtv_reconfigure( this.tvPtr );
+            }
+        }
+
+        private void exitToolStripMenuItem_Click( object sender, EventArgs e ) {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click( object sender, EventArgs e ) {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Bitmap File|*.bmp";
+            DialogResult openResult = openDialog.ShowDialog();
+            if( DialogResult.OK == openResult ) {
+                this.SetPongImage( openDialog.FileName );
             }
         }
     }
